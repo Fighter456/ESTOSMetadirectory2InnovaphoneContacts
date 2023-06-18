8376029e7451ac26f0af2fd8a344114b331fc5f8
Dennis Kraffczyk <dennis.kraffczyk@gmail.com>
2023-06-18 15:24:06 +0200

    Implement first parts of a timer looping through all `*.csv` files

--- ESTOSMetadirectory2InnovaphoneContacts/ESTOSMetadirectory2InnovaphoneContacts.cs
+++ ESTOSMetadirectory2InnovaphoneContacts/ESTOSMetadirectory2InnovaphoneContacts.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.ServiceProcess;
+using System.Timers;
 
 namespace ESTOSMetadirectory2InnovaphoneContacts
 {
@@ -43,10 +45,41 @@ namespace ESTOSMetadirectory2InnovaphoneContacts
                     eventLog1.WriteEntry("Created service directory successful.", EventLogEntryType.Information);
                 }
             }
+
+            Timer timer = new Timer();
+            timer.Interval = 20000;
+            timer.Elapsed += new ElapsedEventHandler(this.OnTick);
+            timer.Start();
         }
 
         protected override void OnStop()
         {
+        }
+
+        public void OnTick(object sender, ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry(
+                "Searching for convertable files in service directory",
+                EventLogEntryType.Information
+            );
+
+            string[] files = Directory.GetFiles(
+                Path.GetPathRoot(Environment.SystemDirectory) + serviceName,
+                "*.csv"
+            );
+
+            eventLog1.WriteEntry(
+                string.Format("Found {0} files for conversion", files.Count()),
+                EventLogEntryType.Information
+            );
+
+            foreach (string item in files)
+            {
+                eventLog1.WriteEntry(
+                    string.Format("Begin conversion of file '{0}'", item),
+                    EventLogEntryType.Information
+                );
+            }
         }
     }
 }
