6182cfd0b3de86221200129343b01c11d88f2c0c
Dennis Kraffczyk <dennis.kraffczyk@gmail.com>
2023-06-25 14:47:30 +0200

    Get the PBX API data from the Windows credential store

--- ESTOSMetadirectory2InnovaphoneContacts/ESTOSMetadirectory2InnovaphoneContacts.cs
+++ ESTOSMetadirectory2InnovaphoneContacts/ESTOSMetadirectory2InnovaphoneContacts.cs
@@ -1,4 +1,5 @@
-﻿using CsvHelper;
+﻿using CredentialManagement;
+using CsvHelper;
 using CsvHelper.Configuration;
 using System;
 using System.Diagnostics;
@@ -13,6 +14,10 @@ namespace ESTOSMetadirectory2InnovaphoneContacts
     public partial class ESTOSMetadirectory2InnovaphoneContacts : ServiceBase
     {
         public EventLog eventLog1;
+        public string httpEndpoint;
+        public string httpUserName;
+        public string httpPassword;
+        public string innovaphoneDomain;
         public const string serviceName = "ESTOSMetadirectory2InnovaphoneContacts";
         public ESTOSMetadirectory2InnovaphoneContacts()
         {
@@ -49,7 +54,80 @@ namespace ESTOSMetadirectory2InnovaphoneContacts
                 }
             }
 
-            Timer timer = new Timer();
+            using (var credential = new Credential())
+            {
+                credential.Target = ServiceName;
+                credential.Load();
+
+                if (String.IsNullOrEmpty(credential.Password))
+                {
+                    eventLog1.WriteEntry(
+                        "Please set the username and password in the windows credential manager.",
+                        EventLogEntryType.Warning
+                    );
+
+                    Stop();
+                }
+                else
+                {
+                    if (credential.Username.Split('|').Length < 2) {
+                        eventLog1.WriteEntry(string.Format(
+                                "Username of the '{0}' credential does not contain the two required parts of endpoint base url and the username splitted by '-'.",
+                                serviceName
+                            ),
+                            EventLogEntryType.Error
+                        );
+
+                        Stop();
+                    }
+
+                    string[] credentialParts = credential.Username.Split('|');
+                    if (!Uri.IsWellFormedUriString(credentialParts[0], UriKind.Absolute) )
+                    {
+                        eventLog1.WriteEntry(string.Format(
+                                "Endpoint url '{0}' does not seems to be a valid url.",
+                                credentialParts[0]
+                            ),
+                            EventLogEntryType.Error
+                        );
+
+                        Stop();
+                    }
+
+                    httpEndpoint = credentialParts[0];
+                    httpPassword = credential.Password;
+                    httpUserName = credentialParts[1];
+
+                    if (credentialParts.Length == 3)
+                    {
+                        innovaphoneDomain = credentialParts[2];
+                    }
+                    else
+                    {
+                        innovaphoneDomain = new Uri(httpEndpoint).Host;
+                    }
+
+                    if (innovaphoneDomain.Split('.').Length >= 3)
+                    {
+                        string[] parts = innovaphoneDomain.Split('.');
+                        Array.Reverse(parts);
+                        innovaphoneDomain = parts[1] + "." + parts[0];
+                    }
+
+                    eventLog1.WriteEntry(string.Format(
+                            "Read '{0}' as innovaphone PBX-endpoint url, " +
+                            "'{1}' as innovaphone PBX-domain " +
+                            "and username '{2}' from windows credential manager.",
+                            httpEndpoint,
+                            innovaphoneDomain,
+                            httpUserName
+                        ),
+                        EventLogEntryType.Information
+                    );
+                }
+            }
+
+            System.Timers.Timer timer = new System.Timers.Timer();
             timer.Interval = 20000;
             timer.Elapsed += new ElapsedEventHandler(this.OnTick);
             timer.Start();
